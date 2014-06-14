f322ad471d796c2be3e60b5f636816a749c50e79
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-14 03:12:53 +0200

    make it a function and add explanation of why

--- test.js
+++ test.js
@@ -3,14 +3,16 @@ var assert = require('assert');
 var ansiRegex = require('./');
 
 it('should match ansi code in a string', function () {
-	assert(ansiRegex.test('\x1b[4mcake\x1b[0m'));
-	assert(ansiRegex.test('\x1b[0m\x1b[4m\x1b[42m\x1b[31mfoo\x1b[39m\x1b[49m\x1b[24mfoo\x1b[0m'));
+	assert(ansiRegex().test('foo\u001b[4mcake\u001b[0m'));
+	assert(ansiRegex().test('\u001b[4mcake\u001b[0m'));
+	assert(ansiRegex().test('foo\u001b[4mcake\u001b[0m'));
+	assert(ansiRegex().test('\u001b[0m\u001b[4m\u001b[42m\u001b[31mfoo\u001b[39m\u001b[49m\u001b[24mfoo\u001b[0m'));
 });
 
 it('should match ansi code from ls command', function () {
-    assert(ansiRegex.test('\x1b[00;38;5;244m\x1b[m\x1b[00;38;5;33mfoo\x1b[0m'));
+	assert(ansiRegex().test('\u001b[00;38;5;244m\u001b[m\u001b[00;38;5;33mfoo\u001b[0m'));
 });
 
 it('should match reset;setfg;setbg;italics;strike;underline sequence in a string', function () {
-	assert(ansiRegex.test('\x1b[0;33;49;3;9;4mbar\x1b[0m'));
+	assert(ansiRegex().test('\u001b[0;33;49;3;9;4mbar\u001b[0m'));
 });
