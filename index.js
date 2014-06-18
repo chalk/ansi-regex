cc29a4aaa9cc225137307622b05be722457d0558
Arjun Mehta <arjunmeht@gmail.com>
2014-06-18 13:50:07 -0400

    revised regex for more supported codes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
 module.exports = function () {
-	return /\u001b\[(?:[0-9]{1,3}(?:;[0-9]{1,3})*)?[m|K]/g;
-  // return /(\x1b\[|\x9b)[^@-_]*[@-_]|\x1b[@-_]/gi;
+  return /(\x1b\[|\x9b)[^@-_]*[@-_]|\x1b[@-_]/gi;
 };
