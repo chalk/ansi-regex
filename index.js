22c1a2cea1573a9f129e90eee5f5f8c44d4a8f7e
Arjun Mehta <arjunmeht@gmail.com>
2014-08-29 14:41:38 -0400

    updating regex to identify mostly just cursor and style codes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
-module.exports = function () {
-  return /(\u001b\[|\u009b)([0-\?]*)?([0-;]*)?([" "-\/]*)?[@-~]|\u001b[@-_]/gi;
+
+module.exports = function (strict) {
+    return /((\u001b\[)|\u009b)(([0-9]{1,3})?((;[0-9]{0,3})*)?[A-M|f-m])|\u001b[A-M]/g;
 };
