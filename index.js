1255fd0654bccb89acc71be6a494de1cfcf74253
Josh Junon <josh@junon.me>
2017-06-20 10:21:24 -0700

    split regex into multiple lines

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
-// eslint-disable-next-line no-control-regex
-module.exports = () => /[\u001b\u009b][[\]()#;?]*(?:(?:(?:[a-zA-Z0-9]*(?:;[a-zA-Z0-9]*)*)?\x07)|(?:(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-PRZcf-ntqry=><~]))/g;
+module.exports = () => {
+	const pattern = [
+		'[\\u001b\\u009b][[\\]()#;?]*(?:(?:(?:[a-zA-Z0-9]*(?:;[a-zA-Z0-9]*)*)?\\x07)',
+		'(?:(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-PRZcf-ntqry=><~]))'
+	].join('|');
+
+	return new RegExp(pattern, 'g');
+};
