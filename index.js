eac826a340f4e292815e4425ee670d42cae38c23
ZSkycat <skycatmew@qq.com>
2018-09-18 16:05:49 +0800

    Add option to only match the first occurrence (#24)
    Fixes #23

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
-module.exports = () => {
+const defaultOptions = {
+	onlyFirst: false
+};
+
+module.exports = options => {
+	options = Object.assign({}, defaultOptions, options);
+
 	const pattern = [
 		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\\u0007)',
 		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
 	].join('|');
 
-	return new RegExp(pattern, 'g');
+	return new RegExp(pattern, options.onlyFirst ? undefined : 'g');
 };
