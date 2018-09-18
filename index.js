385eca95692ba45d6312e7ae532e60bb06e2777d
Qix <Qix-@users.noreply.github.com>
2018-09-18 02:01:35 -0600

    Add scroll escapes (#20)
    Fixes #19

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = () => {
 	const pattern = [
 		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\\u0007)',
-		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))'
+		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
 	].join('|');
 
 	return new RegExp(pattern, 'g');
