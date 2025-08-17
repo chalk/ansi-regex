72bc570aaf25fca25541b49c6a8564f3ec63e835
Sindre Sorhus <sindresorhus@gmail.com>
2025-08-17 23:14:59 +0700

    Simplify regex

--- test.js
+++ test.js
@@ -61,9 +61,92 @@ test('match "change icon name and window title" in string', t => {
 	t.is('\u001B]0;sg@tota:~/git/\u0007\u001B[01;32m[sg@tota\u001B[01;37m misc-tests\u001B[01;32m]$'.match(ansiRegex())[0], '\u001B]0;sg@tota:~/git/\u0007');
 });
 
-test('match colon separated sequence arguments', t => {
+test('match colon-separated sequence arguments', t => {
 	t.regex('\u001B[38:2:68:68:68:48:2:0:0:0m', ansiRegex());
 	t.is('\u001B[38:2:68:68:68:48:2:0:0:0m'.match(ansiRegex())[0], '\u001B[38:2:68:68:68:48:2:0:0:0m');
+});
+
+test('match colon-separated underline variants', t => {
+	for (const code of ['\u001B[4:0m', '\u001B[4:1m', '\u001B[4:2m', '\u001B[4:3m', '\u001B[4:4m', '\u001B[4:5m']) {
+		t.regex(code, ansiRegex());
+		t.is(code.match(ansiRegex())[0], code);
+	}
+});
+
+test('match colon-separated indexed color (38:5)', t => {
+	const code = '\u001B[38:5:123m';
+	t.regex(code, ansiRegex());
+	t.is(code.match(ansiRegex())[0], code);
+});
+
+test('match colon-separated indexed background color (48:5)', t => {
+	const code = '\u001B[48:5:200m';
+	t.regex(code, ansiRegex());
+	t.is(code.match(ansiRegex())[0], code);
+});
+
+test('match colon-separated underline color palette index (58:5)', t => {
+	const code = '\u001B[58:5:200m';
+	t.regex(code, ansiRegex());
+	t.is(code.match(ansiRegex())[0], code);
+});
+
+test('match colon-separated RGB colors (38:2::R:G:B and 48:2::R:G:B)', t => {
+	for (const code of ['\u001B[38:2::12:34:56m', '\u001B[48:2::200:201:202m']) {
+		t.regex(code, ansiRegex());
+		t.is(code.match(ansiRegex())[0], code);
+	}
+});
+
+test('match colon-separated underline color RGB (58:2::R:G:B)', t => {
+	const code = '\u001B[58:2::255:0:0m';
+	t.regex(code, ansiRegex());
+	t.is(code.match(ansiRegex())[0], code);
+});
+
+test('match colon-separated RGBA foreground/background (38:6, 48:6)', t => {
+	for (const code of ['\u001B[38:6::255:0:0:128m', '\u001B[48:6::0:0:0:64m']) {
+		t.regex(code, ansiRegex());
+		t.is(code.match(ansiRegex())[0], code);
+	}
+});
+
+test('colon-separated sequences should not overconsume', t => {
+	const samples = [
+		'\u001B[4:5mX',
+		'\u001B[38:5:123mX',
+		'\u001B[58:2::255:0:0mX',
+		'\u001B[38:2::12:34:56mX',
+		'\u001B[48:2::200:201:202mX',
+	];
+
+	for (const inputString of samples) {
+		const match = inputString.match(ansiRegex())[0];
+		t.truthy(match);
+		t.is(inputString.replace(ansiRegex(), ''), 'X');
+	}
+});
+
+test('does not match bracketed text without ESC', t => {
+	const samples = [
+		'[38:2:68:68:68m',
+		'[4:5m',
+		'some [0m text',
+		'plain [58:2::255:0:0m words',
+	];
+	for (const inputString of samples) {
+		t.is(inputString.match(ansiRegex()), null);
+	}
+});
+
+test('does not match incomplete CSI', t => {
+	const inputString = '\\u001B[';
+	t.is(inputString.match(ansiRegex()), null);
+});
+
+test('does not match ESC followed by unsupported final', t => {
+	const inputString = 'pre\\u001B`post';
+	t.is(inputString.match(ansiRegex()), null);
 });
 
 // Testing against extended codes (excluding codes ending in 0-9)
